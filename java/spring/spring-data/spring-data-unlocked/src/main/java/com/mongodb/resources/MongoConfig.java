1253930ee491a5225ceff8deeaf950c914e72505
Ricardo Mello <ricardohsmello@gmail.com>
2026-08-07 17:30:18 -0300

    Add MongoDB transaction sample with MongoDatabaseFactory and rollback demo

--- java/spring/spring-data/spring-data-unlocked/src/main/java/com/mongodb/resources/MongoConfig.java
+++ java/spring/spring-data/spring-data-unlocked/src/main/java/com/mongodb/resources/MongoConfig.java
@@ -2,14 +2,15 @@ package com.mongodb.resources;
 
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
-import com.mongodb.ReadPreference;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 
 @Configuration
 public class MongoConfig {
@@ -20,19 +21,31 @@ public class MongoConfig {
     @Value("${spring.data.mongodb.uri}")
     private String connectionString;
 
+    @Value("${devrel.application.track-name}")
+    private String appTrackName;
+
     @Bean
     public MongoClient mongoClient() {
         MongoClientSettings settings = MongoClientSettings.builder()
                 .applyConnectionString(new ConnectionString(connectionString))
-                .readPreference(ReadPreference.nearest())
-                .applicationName("devrel-tutorial-spring-data-java")
+                .applicationName(appTrackName)
                 .build();
         return MongoClients.create(settings);
     }
 
 
     @Bean
-    public MongoOperations mongoTemplate(MongoClient mongoClient) {
-        return new MongoTemplate(mongoClient, database);
+    public SimpleMongoClientDatabaseFactory mongoDatabaseFactory(MongoClient mongoClient) {
+        return new SimpleMongoClientDatabaseFactory(mongoClient, database);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate(MongoDatabaseFactory factory) {
+        return new MongoTemplate(factory);
+    }
+
+    @Bean
+    public MongoTransactionManager mongoTransactionManager(MongoDatabaseFactory factory) {
+        return new MongoTransactionManager(factory);
     }
 }
