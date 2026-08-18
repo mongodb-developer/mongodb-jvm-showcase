8e3fc2ed3023a1d613208f93079cd7abfe632af9
Ricardo Mello <ricardohsmello@gmail.com>
2026-08-18 17:43:22 -0300

    Creating agentic wms project

---
+++ java/use-cases/agentic-wms/src/main/java/com/devrel/wms/AgenticWmsApplication.java
@@ -0,0 +1,13 @@
+package com.devrel.wms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenticWmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenticWmsApplication.class, args);
+	}
+
+}
