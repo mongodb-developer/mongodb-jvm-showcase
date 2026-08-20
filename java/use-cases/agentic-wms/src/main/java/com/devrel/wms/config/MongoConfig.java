dcd749a410956ec883d772b878abd4a69f98aa70
Ricardo Mello <ricardohsmello@gmail.com>
2026-08-20 19:04:53 -0300

    Added app track name

--- java/use-cases/agentic-wms/src/main/java/com/devrel/wms/config/MongoConfig.java
+++ java/use-cases/agentic-wms/src/main/java/com/devrel/wms/config/MongoConfig.java
@@ -1,5 +1,7 @@
 package com.devrel.wms.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.mongodb.autoconfigure.MongoClientSettingsBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
@@ -11,5 +13,12 @@ public class MongoConfig {
 	@Bean
 	public MongoTransactionManager mongoTransactionManager(MongoDatabaseFactory mongoDatabaseFactory) {
 		return new MongoTransactionManager(mongoDatabaseFactory);
+	}
+
+	@Bean
+	public MongoClientSettingsBuilderCustomizer trackNameCustomizer(
+			@Value("${devrel.application.track-name}") String trackName) {
+
+		return builder -> builder.applicationName(trackName);
 	}
 }
